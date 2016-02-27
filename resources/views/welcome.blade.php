e365060260ce45e5557cdb2814b4fc81da61bead
Gabe Lorenz <gabelorenz@gmail.com>
2016-02-26 22:40:39 -0500

    updated some sweet sweet images...not the H

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,9 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="Images/H.ico">
 
-    <title>Carousel Template for Bootstrap</title>
+    <title>Hackensack Auto Body</title>
 
     <!-- Bootstrap core CSS -->
     <link href="Stylesheets/bootstrap.min.css" rel="stylesheet">
@@ -33,11 +33,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Project name</a>
+              <a class="navbar-brand" href="#">Hackensack Auto Body</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+                <li class="active"><a href="/">Home</a></li>
                 <li><a href="about">About Us</a></li>
                 <li><a href="towing-services">Towing Services</a></li>
                 <li><a href="auto-body-repair">Auto Body Repairs</a></li>
@@ -113,22 +113,22 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
+          <img class="img-circle" src="Images/TowingServices_icon2.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Towing Services</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="towing-services" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
+          <img class="img-circle" src="Images/Auto_Repair.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Auto Body Repairs</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="auto-body-repair" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Heading</h2>
+          <img class="img-circle" src="Images/Mechanical_repair_icon.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Mechanical Repairs</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <p><a class="btn btn-default" href="mechanical-repair" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 
@@ -141,7 +141,7 @@
     </div><!-- /.container -->
 
     <!-- FOOTER -->
-      <footer style="background-color: rgba(0,0,0,0.3); padding-bottom: 25px;">
+      <footer style="background-color: rgba(0,0,0,0.3); padding-bottom: 25px; border-top: 5px solid #666;">
         <div class="container">
             <div class="col-md-3">
                 <h3>Hours</h3>
@@ -161,11 +161,13 @@
                 <h3>Certification</h3>
             </div>
             <div class="col-md-3">
-                <h3>Contact</h3>
+                <h3 title="office"><i class="glyphicon glyphicon-earphone"></i> (201)-342-4311</h3>
+                <h3 title="towing"><i class="glyphicon glyphicon-road"></i> (201)-342-7240</h3>
+                <p>96 1st Street<br />Hackensack, NJ 07601<br />Fax: (201) 342-5162</p>
             </div>
             <div class="col-md-3">
                 <h3>Location</h3>
-                <a href="https://www.google.com/maps/dir//Hackensack+Auto+Body+Company,+96+1st+Street,+Hackensack,+NJ+07601/"><img src="Images/Google_Maps.PNG"/></a>
+                <a href="https://www.google.com/maps/dir//Hackensack+Auto+Body+Company,+96+1st+Street,+Hackensack,+NJ+07601/"><img src="Images/Google_Maps.PNG" /></a>
             </div>
         </div>
       </footer>
