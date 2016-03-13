85333689a467ad62bfeef76871fc4daa4447abe9
Evan O'Connell <oconn.e@gmail.com>
2016-03-13 16:15:21 -0400

    Create master layout template

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,35 @@
+    <!-- FOOTER -->
+      <footer style="background-color: rgba(0,0,0,0.3); padding-bottom: 25px; border-top: 5px solid #666;">
+        <div class="container">
+            <div class="col-md-3">
+                <h3>Hours</h3>
+                <table>
+                    <tbody>
+                    <tr><td width="100px">Monday</td><td>8AM-5PM</td></tr>
+                    <tr><td>Tuesday</td><td>8AM-5PM</td></tr>
+                    <tr><td>Wednesday</td><td>8AM-5PM</td></tr>
+                    <tr><td>Thursday</td><td>8AM-5PM</td></tr>
+                    <tr><td>Friday</td><td>8AM-5PM</td></tr>
+                    <tr><td>Saturday</td><td>8AM-12PM</td></tr>
+                    <tr><td>Sunday</td><td>Closed</td></tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-3">
+                <h3>Certification</h3>
+                <img src="Images/bbb.png" />
+                <img src="Images/dupont.png" />
+            </div>
+            <div class="col-md-3">
+                <h3 title="office"><i class="glyphicon glyphicon-earphone"></i> (201)-342-4311</h3>
+                <h3 title="towing"><i class="glyphicon glyphicon-road"></i> (201)-342-7240</h3>
+                <p>96 1st Street<br />Hackensack, NJ 07601<br />Fax: (201) 342-5162</p>
+            </div>
+            <div class="col-md-3">
+                <h3>Location</h3>
+                <a href="https://www.google.com/maps/dir//Hackensack+Auto+Body+Company,+96+1st+Street,+Hackensack,+NJ+07601/"><img src="Images/Google_Maps.PNG" /></a>
+            </div>
+        </div>
+      </footer>
+
+    <div class="container"><p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p></div>
