7833a1486c55ba70f6b92db32979a912bbccdb18
Gabe Lorenz <gabelorenz@gmail.com>
2016-04-06 20:57:11 -0400

    Send that brand home yo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Hackensack Auto Body</a>
+              <a class="navbar-brand" href="{{ url('/') }}">Hackensack Auto Body</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
